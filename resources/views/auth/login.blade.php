89e4876da4476fff69bd6e51d481bd6461357395
SamuelParrales <samuel.parrales@outlook.com>
2023-01-25 10:40:06 -0500

    project created

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center mt-4">
+            <div class="col-md-8 col-lg-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h1 class="text-center mb-3">{{ __('Login') }}</h1>
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="row justify-content-center g-0 mb-3">
+                                <div class="form-floating col-md-9 col-lg-8">
+                                    <input id="email" type="email"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="email">
+                                    <label for="email">{{ __('Email Address') }}</label>
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="row justify-content-center g-0 mb-2">
+                                <div class="form-floating  col-md-9 col-lg-8">
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror" name="password"
+                                        required autocomplete="current-password"
+                                        placeholder="password"
+                                        >
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                    <label for="floatingPassword">{{ __('Password') }}</label>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-md-6 offset-md-1 offset-lg-2">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                                            {{ old('remember') ? 'checked' : '' }}>
+                                        <label class="form-check-label" for="remember">
+                                            {{ __('Remember Me') }}
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-main">
+                                        {{ __('Login') }}
+                                    </button>
+
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link btn-link-main text-main" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                    @endif
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
