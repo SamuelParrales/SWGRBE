72f6205ee0e6f3564b75fee1f0980fc9130743f7
SamuelParrales <samuel.parrales@outlook.com>
2023-02-07 16:22:03 -0500

    Add CategorySeeder

--- database/seeders/defaultdata/CategorySeeder.php
+++ database/seeders/defaultdata/CategorySeeder.php
@@ -20,5 +20,42 @@ class CategorySeeder extends Seeder
         $category = new Category();
         $category->name ="Grandes electrodomésticos";
         $category->save();
+
+        $category = new Category();
+        $category->name ="Pequeños electrodomésticos";
+        $category->save();
+
+        $category = new Category();
+        $category->name ="Equipos informáticos y telecomunicaciones";
+        $category->save();
+
+        $category = new Category();
+        $category->name ="Aparatos electrónicos de bajo consumo";
+        $category->save();
+
+        $category = new Category();
+        $category->name ="Aparatos de alumbrado";
+        $category->save();
+
+        $category = new Category();
+        $category->name ="Herramientas eléctricas o electrónicas";
+        $category->save();
+
+        $category = new Category();
+        $category->name ="Juguetes y equipos deportivos o de tiempo libre";
+        $category->save();
+
+        $category = new Category();
+        $category->name ="Aparatos médicos";
+        $category->save();
+
+        $category = new Category();
+        $category->name ="Instrumentos de vigilancia y control";
+        $category->save();
+
+        $category = new Category();
+        $category->name ="Máquinas expendedoras";
+        $category->save();
+
     }
 }
